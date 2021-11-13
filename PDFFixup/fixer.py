d8fd388ec4a2add48b4fa5f77093f62a7f6b377f
Courtenay <hdsheena@users.noreply.github.com>
2021-11-13 11:50:44 -0800

    use line position of characters to classify rows

--- PDFFixup/fixer.py
+++ PDFFixup/fixer.py
@@ -3,7 +3,9 @@ from __future__ import division
 import math
 import pdfminer
 from collections import defaultdict
+import itertools
 
+from pdfminer.high_level import extract_pages,extract_text,extract_text_to_fp
 
 from pdfminer.pdfparser import PDFParser
 from pdfminer.pdfdocument import PDFDocument
@@ -14,6 +16,9 @@ from pdfminer.pdfinterp import PDFPageInterpreter
 
 from pdfminer.layout import LAParams
 from pdfminer.converter import PDFPageAggregator
+
+import matplotlib.pyplot as plt
+from matplotlib import patches
 
 TEXT_ELEMENTS = [
     pdfminer.layout.LTTextBox,
@@ -65,6 +70,23 @@ def get_tables(pdf_path):
     return [page_to_table(page_layout) for page_layout in
             extract_layout_by_page(pdf_path)]
 
+def pairify(it):
+    """find pairs of nearby numbers"""
+    it0, it1 = itertools.tee(it, 2)
+    first = next(it0)
+    return zip(itertools.chain([first, first], it0), it1)
+
+def cluster(sequence, maxgap):
+    """get batches of similar numbers"""
+    batch = []
+    for prev, val in pairify(sequence):
+        if val - prev >= maxgap:
+            yield batch
+            batch = []
+        else:
+            batch.append(val)
+    if batch:
+        yield batch
 
 def page_to_table(page_layout):
     """
@@ -85,60 +107,42 @@ def page_to_table(page_layout):
             other.append(e)
 
     # convert text elements to characters
-    # and rectangles to lines
+
     characters = extract_characters(texts)
-    lines = [cast_as_line(r) for r in rects
-             if width(r) < 2 and
-             area(r) > 1]
-
-    # match each character to a bounding rectangle where possible
-    box_char_dict = {}
+    # get some rows to use for classifying characters
+    box_char_dict_2 = {}
+    last_char_right_bound = 0
+    rows = []
     for c in characters:
-        # choose the bounding box that occurs the majority of times for each of these:
-        bboxes = defaultdict(int)
+        l_y =  c.bbox[1]
+        rows.append(l_y)
+
+    rows = sorted(rows)
+    
+    uniquerows1 = {}
+    for row in cluster(rows,1):
+        print(row[0])
+        for num in row:
+            uniquerows1[num]=row[0]
+      
+    for c in characters:
+        # choose the row closest to this char's y coordinates:
+
         l_x, l_y = c.bbox[0], c.bbox[1]
-        bbox_l = find_bounding_rectangle((l_x, l_y), lines)
-        bboxes[bbox_l] += 1
-
-        c_x, c_y = math.floor((c.bbox[0] + c.bbox[2]) / 2), math.floor((c.bbox[1] + c.bbox[3]) / 2)
-        bbox_c = find_bounding_rectangle((c_x, c_y), lines)
-        bboxes[bbox_c] += 1
-
-        u_x, u_y = c.bbox[2], c.bbox[3]
-        bbox_u = find_bounding_rectangle((u_x, u_y), lines)
-        bboxes[bbox_u] += 1
-
-        # if all values are in different boxes, default to character center.
-        # otherwise choose the majority.
-        if max(bboxes.values()) == 1:
-            bbox = bbox_c
+        l_y_rounded = uniquerows1[l_y]
+        #print(l_y_rounded)
+        if l_y_rounded in box_char_dict_2.keys():
+            box_char_dict_2[l_y_rounded].append(c)
+            #continue
         else:
-            bbox = max(bboxes.items(), key=lambda x: x[1])[0]
-
-        if bbox is None:
+            box_char_dict_2[l_y_rounded] = [c]
             continue
 
-        if bbox in box_char_dict.keys():
-            box_char_dict[bbox].append(c)
-            continue
 
-        box_char_dict[bbox] = [c]
+        last_char_right_bound = c.bbox[2]
+        
 
-    # look for empty bounding boxes by scanning
-    # over a grid of values on the page
-    for x in range(100, 550, 10):
-        for y in range(50, 800, 10):
-            bbox = find_bounding_rectangle((x, y), lines)
-
-            if bbox is None:
-                continue
-
-            if bbox in box_char_dict.keys():
-                continue
-
-            box_char_dict[bbox] = []
-
-    return boxes_to_table(box_char_dict)
+    return boxes_to_table_2(box_char_dict_2,uniquerows1)
 
 
 def flatten(lst):
@@ -166,87 +170,45 @@ def extract_characters(element):
     return []
 
 
-def width(rect):
-    x0, y0, x1, y1 = rect.bbox
-    return min(x1 - x0, y1 - y0)
 
 
-def length(rect):
-    x0, y0, x1, y1 = rect.bbox
-    return max(x1 - x0, y1 - y0)
 
-
-def area(rect):
-    x0, y0, x1, y1 = rect.bbox
-    return (x1 - x0) * (y1 - y0)
-
-
-def cast_as_line(rect):
-    x0, y0, x1, y1 = rect.bbox
-
-    if x1 - x0 > y1 - y0:
-        return (x0, y0, x1, y0, "H")
-    else:
-        return (x0, y0, x0, y1, "V")
-
-
-def does_it_intersect(x, (xmin, xmax)):
-    return (x <= xmax and x >= xmin)
-
-
-def find_bounding_rectangle((x, y), lines):
-    v_intersects = [l for l in lines
-                    if l[4] == "V"
-                    and does_it_intersect(y, (l[1], l[3]))]
-
-    h_intersects = [l for l in lines
-                    if l[4] == "H"
-                    and does_it_intersect(x, (l[0], l[2]))]
-
-    if len(v_intersects) < 2 or len(h_intersects) < 2:
-        return None
-
-    v_left = [v[0] for v in v_intersects
-              if v[0] < x]
-
-    v_right = [v[0] for v in v_intersects
-               if v[0] > x]
-
-    if len(v_left) == 0 or len(v_right) == 0:
-        return None
-
-    x0, x1 = max(v_left), min(v_right)
-
-    h_down = [h[1] for h in h_intersects
-              if h[1] < y]
-
-    h_up = [h[1] for h in h_intersects
-            if h[1] > y]
-
-    if len(h_down) == 0 or len(h_up) == 0:
-        return None
-
-    y0, y1 = max(h_down), min(h_up)
-
-    return (x0, y0, x1, y1)
-
-
-def chars_to_string(chars):
+def chars_to_string_2(chars,uniquerows1):
     if not chars:
         return ""
-    rows = sorted(list(set(c.bbox[1] for c in chars)), reverse=True)
-    text = ""
+    rows = sorted(list(set(uniquerows1[c.bbox[1]] for c in chars)), reverse=True)
+    text = []
+    text_so_far = ""
+    last_char_right_bound = 0 
+    y_position = 0
+    # get a new string appended to the text list wherever the right edge of the previous character box is more than 1 pixel from the left edge of the current character box (ie: put words together)
     for row in rows:
-        sorted_row = sorted([c for c in chars if c.bbox[1] == row], key=lambda c: c.bbox[0])
-        text += "".join(c.get_text() for c in sorted_row)
+        print("New row")
+        sorted_row = sorted([c for c in chars if uniquerows1[c.bbox[1]] == row], key=lambda c: c.bbox[0])
+        for c in sorted_row:
+            #print(c.get_text())
+            
+            if c.bbox[0] - last_char_right_bound > 1:
+                #print("new group of characters")
+                text.append(text_so_far)
+                text_so_far=""
+            text_so_far+=c.get_text()
+            last_char_right_bound = c.bbox[2]
+            y_position = c.bbox[1]
+    text.append(text_so_far)
+    print(text)
+    print("number of rows",len(rows))
     return text
 
-
-def boxes_to_table(box_record_dict):
+def boxes_to_table_2(box_record_dict,uniquerows1):
     boxes = box_record_dict.keys()
-    rows = sorted(list(set(b[1] for b in boxes)), reverse=True)
+
+    rows = sorted(list(set(b for b in boxes)), reverse=True)
+    print(len(rows))
     table = []
     for row in rows:
-        sorted_row = sorted([b for b in boxes if b[1] == row], key=lambda b: b[0])
-        table.append([chars_to_string(box_record_dict[b]) for b in sorted_row])
+        sorted_row = sorted([b for b in boxes if b == row], key=lambda b: b)
+
+        table.append([chars_to_string_2(box_record_dict[b],uniquerows1) for b in sorted_row])
     return table
+
